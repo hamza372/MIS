5a3109664641556c5872b8906521bdee4683c41d
Taimur Shah <taimur38@gmail.com>
2018-10-31 12:34:17 +0500

    quick python server

--- bank_portal/src/components/DeckMap/index.tsx
+++ bank_portal/src/components/DeckMap/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import DeckGL, { ScatterplotLayer } from 'deck.gl'
 import { StaticMap } from 'react-map-gl'
 
-import schoolData from '../Map/narrowed.json'
+import schoolData from './narrowed.json'
 
 const mapbox_token = "pk.eyJ1IjoidGFpbXVyMzgiLCJhIjoiY2pucWZuY3BtMGZ6dTNxcG53NDh1N3lxZyJ9.795xICQFpWXrTJxF10EJfw"
 const mapbox_style_url = "mapbox://styles/taimur38/cjnu7h5pn4jel2rqk4pbo74ke"
@@ -28,8 +28,7 @@ class Map extends React.Component<propTypes, any> {
 
 	render() {
 
-		const data = Object.values(schoolData)
-		console.log(data)
+		const data = Object.entries(schoolData).map(([id, school]) => ({ id, ...school }))
 
 		return <DeckGL
 			initialViewState={initialViewState}
@@ -47,12 +46,12 @@ class Map extends React.Component<propTypes, any> {
 					radiusScale={90}
 					radiusMinPixels={1}
 					radiusMaxPixels={300}
-					getPosition={(d : any) => {
-						return [parseFloat(d.GPS_East), parseFloat(d.GPS_North)]
-					}}
+					getPosition={(d : any) => ([parseFloat(d.GPS_East), parseFloat(d.GPS_North)])}
 					getColor={(d: any) => ([255, 140, 0])}
 					pickable={true}
 					onClick={(item : any) => this.props.onSelect(item)}
+					autoHighlight={true}
+					highlightColor={[0, 140, 255, 255]}
 				/>
 			</DeckGL>
 
--- bank_portal/src/components/Home/index.tsx
+++ bank_portal/src/components/Home/index.tsx
@@ -1,10 +1,17 @@
 import * as React from "react"
-import { Route } from 'react-router-dom'
-//import Map from '~/src/components/Map'
 import Map from '~/src/components/DeckMap'
-//import Map from '~/src/components/MapGL'
+import Sidebar from '~/src/components/Sidebar'
 
-export default class Home extends React.Component {
+
+export default class Home extends React.Component<any, any> {
+
+	constructor(props : any) {
+		super(props);
+
+		this.state = {
+			selected: undefined
+		}
+	}
 
 	onSelect = (item : any) => {
 		console.log("SELECTED", item)
@@ -13,7 +20,8 @@ export default class Home extends React.Component {
 	render() {
 		return <div>
 			Hello
-			<Map onSelect={this.onSelect}/>
+			<Map onSelect={item => this.setState({selected: item.object})}/>
+			<Sidebar selected={this.state.selected} />
 		</div>
 	}
 }
