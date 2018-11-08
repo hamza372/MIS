965bb3b4127f0428d6524a860b85b593c13699a7
Taimur Shah <taimur38@gmail.com>
2018-11-09 02:07:02 +0500

    remove unncessary import

--- web/src/modules/Class/Single/Reports/index.js
+++ web/src/modules/Class/Single/Reports/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import Layout from 'components/Layout'
-
 import { StudentMarks } from 'modules/Student/Single/Marks'
 
 import './style.css'
