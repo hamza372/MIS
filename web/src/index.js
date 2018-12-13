efe9b13a8cf675ec183a51d0f8caa2905e8d9234
Taimur Shah <taimur38@gmail.com>
2018-12-13 17:12:22 +0500

    alert on update + payments fix on student creation

--- web/src/index.js
+++ web/src/index.js
@@ -11,7 +11,7 @@ import Routes from './routes'
 import { saveDB, loadDB } from 'utils/localStorage';
 import Syncr from 'syncr'
 
-const debug_host = 'wss://ab9eae32.ngrok.io'
+const debug_host = 'wss://4b53820d.ngrok.io'
 
 
 const host = process.env.REACT_APP_MIS_HOST || debug_host;
--- web/src/registerServiceWorker.js
+++ web/src/registerServiceWorker.js
@@ -64,7 +64,8 @@ function registerValidSW(swUrl) {
               // At this point, the old content will have been purged and
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
+			  // available; please refresh." message in your web app.
+			  alert("New update is available! Close app and reopen to update.")
               console.log('New content is available; please refresh.');
             } else {
               // At this point, everything has been precached.
