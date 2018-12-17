b3736c34b2a491960cfce9caf31dc8235df0759f
Taimur Shah <taimur38@gmail.com>
2018-12-17 15:12:15 +0500

    individual message timeouts

--- web/src/syncr/index.js
+++ web/src/syncr/index.js
@@ -11,6 +11,7 @@ export default class Syncr {
 		this.ws = undefined;
 		this.pingInterval = undefined;
 		this.dispatch = dispatch;
+		this.message_timeout = 10000;
 
 		this.pending = new Map(); // key: uuid, value: promise
 
@@ -38,7 +39,7 @@ export default class Syncr {
 				this.dispatch(disconnected())
 			}
 			this.cleanup();
-			await sleep(5);
+			await sleep(9000 * Math.random() + 1000);
 			this.connect();
 		}
 
@@ -50,7 +51,13 @@ export default class Syncr {
 			console.log("server", msg.type)
 
 			if(msg.key) {
+				if(!this.pending.has(msg.key)) {
+					console.error("message not found in pending - will not process");
+					return;
+				}
+
 				const promise = this.pending.get(msg.key);
+
 				if(msg.type === "failure") {
 					promise.reject(msg.payload)
 				}
@@ -94,12 +101,21 @@ export default class Syncr {
 		const key = v4();
 		return new Promise((resolve, reject) => {
 
-			this.pending.set(key, {resolve, reject});
+			this.pending.set(key, { resolve, reject });
 
 			this.ws.send(JSON.stringify({
 				key,
 				payload: message
 			}));
+
+			setTimeout(() => {
+				if(this.pending.has(key)) {
+					console.log("MSG TIMEOUT!!!")
+					this.pending.get(key).reject("timeout");
+				}
+				this.pending.delete(key);
+			}, this.message_timeout)
+
 		});
 
 	}
