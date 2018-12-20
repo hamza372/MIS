68978e06b73609d98b7cbd1698c34e2a1c1ad98f
Ali Ahmad <raoaliahmad03@gmail.com>
2018-12-20 15:40:32 +0500

    Resolved Changes

--- web/src/index.js
+++ web/src/index.js
@@ -11,7 +11,7 @@ import Routes from './routes'
 import { saveDB, loadDB } from 'utils/localStorage';
 import Syncr from 'syncr'
 
-const debug_host = 'wss://a02cd48f.ngrok.io'
+const debug_host = 'wss://21a0db2a.ngrok.io'
 
 const host = process.env.REACT_APP_MIS_HOST || debug_host;
 
