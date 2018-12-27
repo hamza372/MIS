2e237d2f295389d7dc30239ff255cfe2d2ddfede
Taimur Shah <taimur38@gmail.com>
2018-12-28 01:16:25 +0500

    scheduling working

--- android/app/src/main/java/pk/org/cerp/mischool/mischoolcompanion/MainActivity.kt
+++ android/app/src/main/java/pk/org/cerp/mischool/mischoolcompanion/MainActivity.kt
@@ -1,6 +1,5 @@
 package pk.org.cerp.mischool.mischoolcompanion
 
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -12,23 +11,19 @@ import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
 import com.beust.klaxon.Klaxon
-import com.evernote.android.job.JobManager
+import com.evernote.android.job.util.support.PersistableBundleCompat
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
 import kotlin.Exception
 
 const val TAG = "MISchool-Companion"
 const val MY_PERMISSIONS_SEND_SMS = 1
+const val filename = "pending_messages.json"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        JobManager.create(this).addJobCreator(SMSDispatcher())
 
         val intent = this.intent
 
@@ -44,6 +39,14 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        Log.d(TAG, "scheduling....")
+        try {
+            SMSJob.scheduleJob()
+        }
+        catch(e : Exception) {
+            Log.e(TAG, e.message)
+        }
+
         Log.d(TAG, dataString)
 
         val json_string = java.net.URLDecoder.decode(dataString.split("=")[1], "UTF-8")
@@ -56,7 +59,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         // open file, append messages and quit
-        // task which runs every minute
+        // task which runs every minute will consume from here
+        // do I need to acquire a lock on this file?
 
         try {
             appendMessagesToFile(parsed.messages)
@@ -65,34 +69,6 @@ class MainActivity : AppCompatActivity() {
             Log.e(TAG, e.message)
             Log.e(TAG, e.toString())
         }
-
-
-        // this logic should be happening inside the task.
-        // it means we need to save the queued up messages somewhere to be accessed by the
-        // worker later
-        /*
-        val history = sentMessages()
-        val last_min_messages = history.first
-        val last_15_min_messages = history.second
-
-        val num_messages = parsed.messages.size
-
-        val max_per_minute = 25
-
-        when {
-            last_min_messages > max_per_minute -> null // do nothing, wait another minute.
-            (last_min_messages + num_messages) < max_per_minute -> sendAllSMS(parsed.messages) // fire the messages off immediately.
-            (last_15_min_messages + num_messages) in 30..199 -> null // we don't need to worry about the pta rule, so fire off max per minute this round.
-            (num_messages + last_15_min_messages) > 200 -> null // fire the messages off at a rate that cares about the pta limit (200 / 15 min) 12 per minute...
-        }
-        */
-
-
-        /*
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(parsed.return_link)
-        startActivity(intent)
-        */
 
         finish()
 
@@ -105,11 +81,17 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "appending messages to file.....")
 
         val path = filesDir
-        val file = File(path, "pending_messages.json")
+        val file = File(path, filename)
 
         Log.d(TAG, file.absolutePath)
 
         var content : String? = null
+
+        /*
+        if(file.exists()) {
+            file.delete()
+        }
+        */
 
         if(file.exists()) {
             val bytes = file.readBytes()
@@ -127,60 +109,13 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "new list is $new_list")
 
-        // now write to file...
-
         val res = Klaxon().toJsonString(new_list)
         file.writeBytes(res.toByteArray())
 
-        Log.d(TAG, "DONE writing stupid shit")
+        Log.d(TAG, "DONE writing")
 
     }
 
-    fun sentMessages() : Pair<Int, Int> {
-
-        val unixTime = System.currentTimeMillis()
-
-        try {
-
-            val min_time = unixTime - (15 * 60 * 1000)
-            val cursor = contentResolver.query(
-                    Uri.parse("content://sms/sent"),
-                    arrayOf("date"),
-                    "date > $min_time",
-                    null,
-                    null
-            )
-
-
-            return if (cursor.moveToFirst()) {
-                var messages_past_minute = 0
-                var messages_past_15_min = 0
-
-                do {
-
-                    val date = cursor.getLong(cursor.getColumnIndex("date"))
-                    val diff = (unixTime - date) / 1000L
-
-                    if(diff <= 60) messages_past_minute++
-
-                    messages_past_15_min++
-
-
-                } while (cursor.moveToNext())
-
-                return Pair(messages_past_minute, messages_past_15_min)
-            } else {
-                Log.d(TAG, "couldnt move to first...")
-                return Pair(0, 0)
-            }
-
-        }
-        catch(e : Exception) {
-            Log.e(TAG, e.message)
-            return Pair(0, 0)
-        }
-
-    }
 
     fun permissions() {
 
