d49f07fa59c50db6c74e726eda178c296a0a7abc
ali ahmad <raoaliahmad03@gmail.com>
2019-01-25 15:35:42 +0500

    merge

--- web/src/index.js
+++ web/src/index.js
@@ -11,7 +11,7 @@ import Routes from './routes'
 import { saveDB, loadDB } from 'utils/localStorage';
 import Syncr from 'syncr'
 
-const debug_host = 'wss://90fe7d5e.ngrok.io'
+const debug_host = 'wss://34cea1b2.ngrok.io'
 
 const host = window.api_url || debug_host;
 
