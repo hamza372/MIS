4201140a01223e1ebe19c6404e310a7eb5fee53a
Ali Ahmad <raoaliahmad03@gmail.com>
2019-03-13 18:52:49 +0500

    Analytics improvements (#110)
    * Improved fees
    
    * added no print
    
    * also changed fees

--- web/src/index.js
+++ web/src/index.js
@@ -11,7 +11,7 @@ import Routes from './routes'
 import { saveDB, loadDB } from 'utils/localStorage';
 import Syncr from 'syncr'
 
-const debug_host = 'wss://781bf10e.ngrok.io';
+const debug_host = 'wss://26cffec2.ngrok.io';
 
 const host = window.api_url || debug_host;
 
