68652933a56408adb3557c459c7fb20d9bb9330b
ali ahmad <raoaliahmad03@gmail.com>
2019-09-18 19:11:10 +0500

    made table editable

--- dashboard/src/utils/getEndPointResource.ts
+++ dashboard/src/utils/getEndPointResource.ts
@@ -1,5 +1,5 @@
 
-//const END_POINT_URL = "https://localhost:8080/dashboard/"
+//const END_POINT_URL = "http://localhost:8080/dashboard/"
 const END_POINT_URL = "https://mis-socket.metal.fish/dashboard/"
 
 export const getEndPointResource = ( point:string, school_id: string, start_date: number, end_date: number) => {
@@ -8,6 +8,7 @@ export const getEndPointResource = ( point:string, school_id: string, start_date
 
 	// @ts-ignore
 	headers.set('Authorization', 'Basic ' + btoa(`${window.username}:${window.password}`))
+	//headers.set('Authorization', 'Basic ' + btoa(`${"cerp"}:${"***REMOVED***"}`))
 
 	return fetch(`${END_POINT_URL}${point}?school_id=${school_id}&start_date=${start_date}&end_date=${end_date}`, {
 		headers
