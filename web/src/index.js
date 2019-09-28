8390081b9a068842d69b601c947efd8e9ac6c4b7
Taimur Shah <taimur38@gmail.com>
2019-09-28 22:17:18 +0500

    redo web

--- web/src/index.js
+++ web/src/index.js
@@ -11,7 +11,7 @@ import Routes from './routes'
 import { saveDB, loadDB } from 'utils/localStorage';
 import Syncr from 'syncr'
 
-const debug_host = 'wss://e78e9dd6.ngrok.io';
+const debug_host = 'wss://6aaad25d.ngrok.io';
 
 const host = window.api_url || debug_host;
 
