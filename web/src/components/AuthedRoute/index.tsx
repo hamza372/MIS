b5fe61e6ffc90a6de12744cf7c850b39abdf5267
Mudassar045 <mudassaralics007@gmail.com>
2019-11-14 13:19:57 +0500

    convert to typescript and add hook to scroll to top

---
+++ web/src/components/AuthedRoute/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+type propsType = {
+	component: any,	
+	school_id: string
+	name: string
+	token: string
+	initialized: boolean
+}
+ 
+const AuthedRoute = ({ component, school_id, name, token, initialized, ...rest }: propsType) => {
+
+	// react's hook
+	useEffect(() => {
+		window.scroll(0, 0)
+	})
+
+	if (!initialized) {
+		return <div>Loading Database....</div>
+	}
+
+	if(token && name) {
+		return <Route component={component} {...rest} />
+	}
+	else if(token) {
+		return <Redirect to="/login" />
+	}
+	else {
+		return <Redirect to="/school-login" />
+	}
+}
+
+export default connect((state: RootReducerState) => ({ ...state.auth, initialized: state.initialized }))(AuthedRoute);
