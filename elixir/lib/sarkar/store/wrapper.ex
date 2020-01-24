94a64f3ec40ed523ff83c337086afb3655b5ff79
Taimur Shah <taimur38@gmail.com>
2020-01-24 13:14:33 +0500

    remove logs

--- elixir/lib/sarkar/store/wrapper.ex
+++ elixir/lib/sarkar/store/wrapper.ex
@@ -1,7 +1,6 @@
 defmodule Sarkar.DB.Postgres do
 	
 	def query(db, querystring, params, opts \\ []) do
-		IO.inspect opts 
 		Postgrex.query(db, querystring, params, pool: DBConnection.Poolboy)
 	end
 end
