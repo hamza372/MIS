5171a69ba8c328fb567e32f5add381eb3dfcd3e0
ali ahmad <raoaliahmad03@gmail.com>
2020-02-01 22:50:18 +0500

    dashboard bug fix

--- dashboard/src/actions/index.ts
+++ dashboard/src/actions/index.ts
@@ -153,7 +153,8 @@ export const updateSchoolInfo = (school_id: string, student_limit: number, paid:
 		id: state.auth.id,
 		payload: {
 			school_id,
-			merges
+			merges,
+			paid
 		}
 	})
 	.then((res: {token: string, sync_state: SyncState }) => {
