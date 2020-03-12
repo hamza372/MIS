71d4d7f7cd6c77fa6514b324591626c67e95358a
Mudassar045 <mudassaralics007@gmail.com>
2020-03-12 11:55:53 +0500

    code refactoring

--- web/src/modules/Student/List/index.tsx
+++ web/src/modules/Student/List/index.tsx
@@ -16,6 +16,263 @@ import { chunkify } from 'utils/chunkify'
 
 import './style.css'
 
+type P = {
+	students: RootDBState["students"]
+	classes: RootDBState["classes"]
+	settings: RootDBState["settings"]
+	schoolLogo?: string
+	forwardTo: string
+	max_limit?: number
+}
+
+type S = {
+	showActiveStudent: boolean
+	showInactiveStudent: boolean
+	printStudentCard: boolean
+	tag: string
+	section_id: string
+}
+
+const CHUNK_SIZE = 29
+
+export class StudentList extends Component<P, S> {
+
+	former: Former
+	constructor(props: P) {
+		super(props)
+
+		this.state = {
+			showActiveStudent: true,
+			showInactiveStudent: false,
+			printStudentCard: false,
+			tag: "",
+			section_id: ""
+		}
+		this.former = new Former(this, [])
+	}
+
+	uniqueTags = (students: RootDBState["students"]): Set<string> => {
+
+		const tags = new Set<string>()
+
+		Object.values(students)
+			.filter(s => s.id && s.Name)
+			.forEach(s => {
+				Object.keys(s.tags || {})
+					.forEach(tag => tags.add(tag))
+			})
+
+		return tags
+	}
+
+	getListFilterCondition = (item: MISStudent) => {
+
+		const { showActiveStudent, showInactiveStudent, tag } = this.state
+
+		//Active is checked and inactive is unchecked
+		if (showActiveStudent && !showInactiveStudent && tag === "") {
+			return item.Active
+			//Show only Active
+		}
+
+		//Active is checked and inactive is unchecked
+		if (showActiveStudent && !showInactiveStudent && tag) {
+
+			if (item.tags === undefined) {
+				return false
+			}
+
+			//Show showActiveStudent with selected tag
+			return item.Active && item.tags[tag]
+		}
+
+		//Active is checked and inactive is checked
+		if (showActiveStudent && showInactiveStudent && tag === "") {
+			return true
+			//show All
+		}
+
+		//Active is checked and inactive is checked
+		if (showActiveStudent && showInactiveStudent && tag) {
+
+			if (item.tags === undefined) {
+				return false
+			}
+			//show all with selected tag
+			return item.tags[tag]
+		}
+
+		//Active is unchecked and inactive is checked
+		if (!showActiveStudent && showInactiveStudent && tag === "") {
+			//show only InActive
+			return !item.Active
+		}
+
+		//Active is unchecked and inactive is checked
+		if (!showActiveStudent && showInactiveStudent && tag) {
+
+			if (item.tags === undefined) {
+				return false
+			}
+
+			//Show InActive with selected tag
+			return !item.Active && item.tags[tag]
+		}
+
+	}
+
+	getSectionName = (sections: AugmentedSection[]): string => {
+		const { section_id } = this.state
+		const section = sections.find(section => section.id === section_id)
+		return section ? section.namespaced_name : ""
+	}
+
+	render() {
+
+		const { classes, students, settings, forwardTo, max_limit } = this.props
+		const { section_id, showActiveStudent, showInactiveStudent, printStudentCard } = this.state
+
+		const schoolSession = {
+			startYear: settings && settings.schoolSession ? moment(settings.schoolSession.start_date).format("YYYY") : "",
+			endYear: settings && settings.schoolSession ? moment(settings.schoolSession.end_date).format("YYYY") : ""
+		}
+
+		const sections = getSectionsFromClasses(classes)
+		const section_name = this.getSectionName(sections)
+
+		let items = Object.entries(students)
+			.filter(([, s]) => s && s.Name &&
+				(forwardTo === "prospective-student" || this.getListFilterCondition(s)) &&
+				(section_id ? s.section_id === section_id : true))
+			.sort(([, a], [, b]) => a.Name.localeCompare(b.Name))
+			.map(([id, student]) => {
+				const relevant_section = sections.find(section => student.section_id === section.id)
+				return {
+					...student,
+					section: relevant_section,
+					id,
+					forwardTo
+				}
+			})
+
+		if (section_id.length === 0) {
+			items = items.sort((a, b) => {
+				const aYear = a.section ? a.section.classYear : 0
+				const bYear = b.section ? b.section.classYear : 0
+
+				return aYear - bYear
+			})
+		}
+
+		let create = '/student/new'
+		let createText = "Add New Student"
+
+		if (forwardTo === 'marks' || forwardTo === 'certificates') {
+			create = ''
+		}
+
+		if (getStudentLimt(students, max_limit)) {
+			create = ''
+		}
+
+		if (forwardTo === "prospective-student") {
+			create = "/student/prospective-student/new"
+			createText = "New Prospective Student"
+			items = items.filter(s => (s.tags !== undefined) && (s.tags["PROSPECTIVE"]))
+		} else {
+			items = items.filter(s => (s.tags === undefined || !s.tags["PROSPECTIVE"]))
+		}
+
+		if (forwardTo === 'payment') {
+			create = '/fees/manage'
+			createText = "Manage Fees"
+		}
+
+		return <div className="student-list">
+			<div className="title no-print">All Students</div>
+			<div className="no-print">
+				{
+					//@ts-ignore
+					<Card
+						items={items}
+						Component={StudentItem}
+						create={create}
+						createText={createText}
+						toLabel={toLabel}>
+
+						{forwardTo !== "prospective-student" && <div className="row filter-container no-print">
+							<div className="row checkbox-container">
+								<div className="checkbox">
+									<input type="checkbox" {...this.former.super_handle(["showActiveStudent"])} />
+									Active
+							</div>
+								<div className="checkbox">
+									<input type="checkbox" {...this.former.super_handle(["showInactiveStudent"])} />
+									InActive
+							</div>
+								<div className="checkbox">
+									<input type="checkbox" {...this.former.super_handle(["printStudentCard"])} />
+									Cards
+							</div>
+							</div>
+							<div className="row">
+								<select className="list-select" {...this.former.super_handle(["tag"])} style={{ marginLeft: 0 }}>
+									<option value="">Select Tag</option>
+									{
+										[...this.uniqueTags(students).keys()]
+											.filter(tag => tag !== "PROSPECTIVE" && (showActiveStudent &&
+												!showInactiveStudent ? tag !== "FINISHED_SCHOOL" : true))
+											.map(tag => <option key={tag} value={tag}> {tag} </option>)
+									}
+								</select>
+								<select className="list-select" {...this.former.super_handle(["section_id"])}>
+									<option value="">Select Class</option>
+									{
+										sections
+											.sort((a, b) => a.classYear - b.classYear)
+											.map(section => <option key={section.id} value={section.id}> {section.namespaced_name} </option>)
+									}
+								</select>
+								<div className="print button" onClick={() => window.print()}>Print</div>
+							</div>
+						</div>}
+					</Card>
+				}
+			</div>
+
+			{	// for first table, Sr. no will start from 1,
+				// for other tables, Sr. no will start from chunkSize * index
+				// here's "index" representing table number
+				!printStudentCard ?
+					chunkify(items, CHUNK_SIZE)
+						.map((chunkItems: AugmentedStudent[], index: number) => <StudentPrintableList students={chunkItems} key={index}
+							chunkSize={index === 0 ? 0 : CHUNK_SIZE * index}
+							schoolName={settings.schoolName}
+							studentClass={section_name} />)
+					:
+					// print 10 students ID cards per page
+					chunkify(items, 8)
+						.map((chunkItems: AugmentedStudent[], index: number) => <StudenPrintableIDCardList students={chunkItems} key={index}
+							schoolName={settings.schoolName}
+							schoolLogo={this.props.schoolLogo}
+							studentClass={section_name}
+							schoolSession={schoolSession} />)
+			}
+
+		</div>
+	}
+}
+
+export default connect((state: RootReducerState, { location, forwardTo = undefined }: { location: RouteProps["location"]; forwardTo: string | undefined }) => ({
+	students: state.db.students,
+	classes: state.db.classes,
+	settings: state.db.settings,
+	schoolLogo: state.db.assets ? state.db.assets.schoolLogo || "" : "",
+	forwardTo: forwardTo || queryString.parse(location.search).forwardTo || "profile",
+	max_limit: state.db.max_limit || -1
+}))(LayoutWrap(StudentList))
+
+
 const StudentItem = (student: AugmentedStudent) => {
 
 	const section_name = student.section ? student.section.namespaced_name : "No Class"
@@ -80,261 +337,9 @@ const StudentItem = (student: AugmentedStudent) => {
 	</div>
 }
 
-const toLabel = (student: AugmentedStudent) => {
-
-	const section_name = student.section ? student.section.namespaced_name : "No Class";
-	const admissionNumber = student.AdmissionNumber ? `a${student.AdmissionNumber}` : "";
-	const phone = student.Phone;
-	return student.Name + student.ManName + section_name + admissionNumber + phone;
-
+const toLabel = (student: AugmentedStudent): string => {
+	const section_name = student.section ? student.section.namespaced_name : "No Class"
+	const admissionNumber = student.AdmissionNumber ? `a${student.AdmissionNumber}` : ""
+	const phone = student.Phone
+	return student.Name + student.ManName + section_name + admissionNumber + phone
 }
-
-type P = {
-	students: RootDBState["students"]
-	classes: RootDBState["classes"]
-	settings: RootDBState["settings"]
-	schoolLogo?: string
-	forwardTo: string
-	max_limit?: number
-}
-
-type S = {
-	showActiveStudent: boolean
-	showInactiveStudent: boolean
-	printStudentCard: boolean
-	tag: string
-	selected_section_id: string
-}
-
-export class StudentList extends Component<P, S> {
-
-	former: Former
-	constructor(props: P) {
-		super(props)
-
-		this.state = {
-			showActiveStudent: true,
-			showInactiveStudent: false,
-			printStudentCard: false,
-			tag: "",
-			selected_section_id: ""
-		}
-		this.former = new Former(this, [])
-	}
-
-	uniqueTags = (students: RootDBState["students"]): Set<string> => {
-
-		const tags = new Set<string>()
-
-		Object.values(students)
-			.filter(s => s.id && s.Name)
-			.forEach(s => {
-				Object.keys(s.tags || {})
-					.forEach(tag => tags.add(tag))
-			})
-
-		return tags
-	}
-
-	getListFilterCondition = (item: MISStudent) => {
-
-		//Active is checked and inactive is unchecked
-		if (this.state.showActiveStudent && !this.state.showInactiveStudent && this.state.tag === "") {
-			return item.Active
-			//Show only Active
-		}
-
-		//Active is checked and inactive is unchecked
-		if (this.state.showActiveStudent && !this.state.showInactiveStudent && this.state.tag !== "") {
-
-			if (item.tags === undefined) {
-				return false
-			}
-			return item.Active && Object.keys(item.tags).includes(this.state.tag)
-			//Show showActiveStudent with selected tag
-		}
-
-		//Active is checked and inactive is checked
-		if (this.state.showActiveStudent && this.state.showInactiveStudent && this.state.tag === "") {
-			return true
-			//show All
-		}
-
-		//Active is checked and inactive is checked
-		if (this.state.showActiveStudent && this.state.showInactiveStudent && this.state.tag !== "") {
-
-			if (item.tags === undefined) {
-				return false
-			}
-			return Object.keys(item.tags).includes(this.state.tag)
-			//show all with selected tag
-		}
-
-		//Active is unchecked and inactive is checked
-		if (!this.state.showActiveStudent && this.state.showInactiveStudent && this.state.tag === "") {
-			return !item.Active
-			//show only InActive
-		}
-
-		//Active is unchecked and inactive is checked
-		if (!this.state.showActiveStudent && this.state.showInactiveStudent && this.state.tag !== "") {
-
-			if (item.tags === undefined) {
-				return false
-			}
-			return !item.Active && Object.keys(item.tags).includes(this.state.tag)
-			//Show InActive with selected tag
-		}
-
-	}
-
-	getSectionName = (sections: AugmentedSection[]): string => {
-		const section = sections.find(section => section.id === this.state.selected_section_id)
-		return section ? section.namespaced_name : ""
-	}
-
-	render() {
-
-		const { classes, students, settings, forwardTo, max_limit } = this.props
-
-		const schoolSession = {
-			startYear: settings && settings.schoolSession ? moment(settings.schoolSession.start_date).format("YYYY") : "",
-			endYear: settings && settings.schoolSession ? moment(settings.schoolSession.end_date).format("YYYY") : ""
-		}
-
-		const sections = getSectionsFromClasses(classes)
-		const section_name = this.getSectionName(sections)
-		const chunkSize = 29 // students per page on printsheet
-
-		let items = Object.entries(students)
-			.filter(([, s]) => s.id && s.Name &&
-				(forwardTo === "prospective-student" || this.getListFilterCondition(s)) &&
-				(this.state.selected_section_id !== "" ? s.section_id === this.state.selected_section_id : true)) // hiding the error for now.... need to build reporting mechanism
-			.sort(([, a], [, b]) => a.Name.localeCompare(b.Name))
-			.map(([id, student]) => {
-				const relevant_section = sections.find(section => student.section_id === section.id)
-				return {
-					...student,
-					section: relevant_section,
-					id,
-					forwardTo
-				}
-			});
-
-		if (this.state.selected_section_id.length === 0) {
-			items = items.sort((a, b) => {
-				const aYear = a.section ? a.section.classYear : 0
-				const bYear = b.section ? b.section.classYear : 0
-
-				return aYear - bYear
-			})
-		}
-
-		let create = '/student/new'
-		let createText = "Add new Student"
-
-		if (forwardTo === 'marks' || forwardTo === 'certificates') {
-			create = '';
-		}
-
-		if (getStudentLimt(students, max_limit)) {
-			create = ''
-		}
-
-		if (forwardTo === "prospective-student") {
-			create = "/student/prospective-student/new"
-			createText = "New Prospective Student"
-			items = items.filter(s => (s.tags !== undefined) && (s.tags["PROSPECTIVE"]))
-		} else {
-			items = items.filter(s => (s.tags === undefined || !s.tags["PROSPECTIVE"]))
-		}
-
-		if (forwardTo === 'payment') {
-			create = '/fees/manage'
-			createText = "Manage Fees"
-		}
-
-		return <div className="student-list">
-			<div className="title no-print">All Students</div>
-			<div className="no-print">
-				{
-					//@ts-ignore
-					<Card
-						items={items}
-						Component={StudentItem}
-						create={create}
-						createText={createText}
-						toLabel={toLabel}>
-
-						{forwardTo !== "prospective-student" && <div className="row filter-container no-print">
-							<div className="row checkbox-container">
-								<div className="checkbox">
-									<input type="checkbox" {...this.former.super_handle(["showActiveStudent"])} style={{ height: "20px" }} />
-									Active
-							</div>
-								<div className="checkbox">
-									<input type="checkbox" {...this.former.super_handle(["showInactiveStudent"])} style={{ height: "20px" }} />
-									InActive
-							</div>
-								<div className="checkbox">
-									<input type="checkbox" {...this.former.super_handle(["printStudentCard"])} style={{ height: "20px" }} />
-									ID Cards
-							</div>
-							</div>
-							<div className="row">
-								<select className="list-select" {...this.former.super_handle(["tag"])} style={{ marginLeft: 0 }}>
-									<option value="">Select Tag</option>
-									{
-										[...this.uniqueTags(students).keys()]
-											.filter(tag => tag !== "PROSPECTIVE" && (this.state.showActiveStudent &&
-												!this.state.showInactiveStudent ? tag !== "FINISHED_SCHOOL" : true))
-											.map(tag => <option key={tag} value={tag}> {tag} </option>)
-									}
-								</select>
-								<select className="list-select" {...this.former.super_handle(["selected_section_id"])}>
-									<option value="">Select Class</option>
-									{
-										sections
-											.sort((a, b) => a.classYear - b.classYear)
-											.map(section => <option key={section.id} value={section.id}> {section.namespaced_name} </option>)
-									}
-								</select>
-								<div className="print button" onClick={() => window.print()}>Print</div>
-							</div>
-						</div>}
-					</Card>
-				}
-			</div>
-
-			{	// for first table, Sr. no will start from 1,
-				// for other tables, Sr. no will start from chunkSize * index
-				// here's "index" representing table number
-				!this.state.printStudentCard ?
-					chunkify(items, chunkSize)
-						.map((chunkItems: AugmentedStudent[], index: number) => <StudentPrintableList students={chunkItems} key={index}
-							chunkSize={index === 0 ? 0 : chunkSize * index}
-							schoolName={settings.schoolName}
-							studentClass={section_name} />)
-					:
-					// print 10 students ID cards per page
-					chunkify(items, 8)
-						.map((chunkItems: AugmentedStudent[], index: number) => <StudenPrintableIDCardList students={chunkItems} key={index}
-							schoolName={settings.schoolName}
-							schoolLogo={this.props.schoolLogo}
-							studentClass={section_name}
-							schoolSession={schoolSession} />)
-			}
-
-		</div>
-	}
-}
-
-
-export default connect((state: RootReducerState, { location, forwardTo = undefined }: { location: RouteProps["location"]; forwardTo: string | undefined }) => ({
-	students: state.db.students,
-	classes: state.db.classes,
-	settings: state.db.settings,
-	schoolLogo: state.db.assets ? state.db.assets.schoolLogo || "" : "",
-	forwardTo: forwardTo || queryString.parse(location.search).forwardTo || "profile",
-	max_limit: state.db.max_limit || -1
-}))(LayoutWrap(StudentList));
