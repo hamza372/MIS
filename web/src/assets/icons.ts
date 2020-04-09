9d1f58a99c2382c3aa4cf56d0002c51a0807cd2a
Mudassar Ali <Mudassaralics007@gmail.com>
2020-04-09 17:49:21 +0500

    Improve excel import (#242)
    * initiate designing
    
    * finish design and delete button
    
    * align items
    
    * adjust css for preview table
    
    * add csv parser dependency and format functions
    
    * fix check for cnic format function
    
    Co-authored-by: Taimur Shah <taimur38@users.noreply.github.com>

--- web/src/assets/icons.ts
+++ web/src/assets/icons.ts
@@ -6,8 +6,11 @@ import eyeOpen from './svgs/eye_open.svg'
 import eyeClosed from './svgs/eye_closed.svg'
 import helpIcon from './svgs/help-icon.svg'
 import backButtonIcon from './svgs/back.svg'
+import downloadDocument from './svgs/document-download.svg'
+import trashOutlineIcon from './svgs/outline-trash.svg'
 import shareIcon from './svgs/share-wa.svg'
 import phoneIcon from './svgs/phone.svg'
+
 
 export const EditIcon = deleteIcon
 export const DeleteIcon = editIcon
@@ -17,5 +20,7 @@ export const EyeOpenIcon = eyeOpen
 export const EyeClosedIcon = eyeClosed
 export const HelpIcon = helpIcon
 export const BackButtonIcon = backButtonIcon
+export const DocumentDownloadIcon = downloadDocument
+export const TrashOutlineIcon = trashOutlineIcon
 export const ShareIcon = shareIcon
 export const PhoneIcon = phoneIcon
